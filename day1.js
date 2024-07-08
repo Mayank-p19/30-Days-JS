c9df63196b5dc5dfba7f784f81af987278327417
Mayank-p19 <mayankpunia74@gmail.com>
2024-07-08 21:56:15 +0530

    updated day1

--- day1.js
+++ day1.js
@@ -1,5 +1,5 @@
 // Functions and Implementations
-// Array Methods - Filter,Find, include, map, forEach, slice, splice
+// Array Methods - Filter,Find, include, map, forEach, slice, splice, spread 
 // How to iterate in array - for, forEach, map
 // Map and methods, Object and its methods
 
@@ -141,19 +141,65 @@
 //     console.log(`Element at index ${i} = ${nums[i]}`);
 // }
 
-const people = [
-    { name: 'Alice', age: 30 },
-    { name: 'Bob', age: 25 },
-    { name: 'Charlie', age: 35 }
-];
+// const people = [
+//     { name: 'Alice', age: 30 },
+//     { name: 'Bob', age: 25 },
+//     { name: 'Charlie', age: 35 }
+// ];
 
-  const filterAge  = people.filter(person => person.age > 30);
-  console.log(filterAge);
+//   const filterAge  = people.filter(person => person.age > 30);
+//   console.log(filterAge);
 
-const users = [
-    { id: 1, name: 'Alice' },
-    { id: 2, name: 'Bob' },
-    { id: 3, name: 'Charlie' }
-];
-  const findId = users.find(person => person.id == 2);
-  console.log(findId);
+// const users = [
+//     { id: 1, name: 'Alice' },
+//     { id: 2, name: 'Bob' },
+//     { id: 3, name: 'Charlie' }
+// ];
+//   const findId = users.find(person => person.id == 2);
+//   console.log(findId);
+
+// const fruits = ['apple', 'banana', 'grapes'];       // creating a new array using spread operator
+// const moreFruits = [...fruits];
+
+// console.log(moreFruits);
+
+// const vegetables = ['carrot', 'tomato'];            // combining two arrays using spread
+
+// const food = [...fruits, ...vegetables];
+// console.log(food);
+
+// const numbers = [1,2,3];                            
+// const sum = (a,b,c) => a+b+c;
+// console.log(sum(...numbers));
+
+// const arr1 = [1,2,3];                               //Merging arrays and removing duplicates
+// const arr2 = [3,4,5];
+// const merged = [...new Set([...arr1, ...arr2])];
+// console.log(merged)
+
+// const user = {id: 1, name: 'Mayank', age: '25'}     //updating objects
+// const updatedUser = {...user, age: '22'}
+// console.log(updatedUser);
+
+// const nums = [1,2,3,4];                             //using spread operator in function calls
+// const max = Math.max(...nums);
+// console.log(max);
+
+// const array = [1,2,3,4,5];                          // for loop method
+//  for(let i=0; i<array.length; i++){
+//     console.log(array[i]);
+//  }
+
+//  array.forEach((num) => {                           // forEach method
+//     console.log(num);
+//  })
+
+//  array.map((num) => {
+//     console.log(num);
+//  })
+
+//  const newArray = array.map((double) => {
+//     return double * 2;
+//  });
+
+//  console.log(newArray);
