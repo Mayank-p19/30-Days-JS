8ee34b4e8962724058af6f3ad53e5b312d8ebf76
Mayank-p19 <mayankpunia74@gmail.com>
2024-07-13 10:05:35 +0530

    routes

--- day5/day5.js
+++ day5/day5.js
@@ -3,11 +3,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const indexRouter = require("./routes/index");
 
-app.get('/', (req,res) =>{
-    res.send("Hello World");
-});
+app.get('/', indexRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-})
+});
