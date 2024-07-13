ea3cc3dcfb2f47d23a4e3351fd35c21a278bf9d4
Mayank-p19 <mayankpunia74@gmail.com>
2024-07-13 10:15:52 +0530

    ejs

--- day5/day5.js
+++ day5/day5.js
@@ -4,8 +4,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const indexRouter = require("./routes/index");
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', indexRouter);
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
